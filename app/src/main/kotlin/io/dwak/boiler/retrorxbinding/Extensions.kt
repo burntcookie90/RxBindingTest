1fdbea9e621b7b4e154874d6943735561bb7ea35
Vishnu Rajeevan <vrajeevan@roundarch.com>
2015-08-05 15:56:52 -0500

    fix view extension

--- app/src/main/kotlin/io/dwak/boiler/retrorxbinding/Extensions.kt
+++ app/src/main/kotlin/io/dwak/boiler/retrorxbinding/Extensions.kt
@@ -1,10 +1,15 @@
 package io.dwak.boiler.retrorxbinding
 
-import android.content.Context
+import android.app.Activity
+import android.support.v4.app.Fragment
 import rx.Observable
 import rx.functions.Action1
 
-public fun Context.bind<T>(from: Observable<T>, to: Action1<in T>) {
+public fun Activity.bind<T>(from: Observable<T>, to: Action1<in T>) {
+    from.subscribe(Action1 { to.call(it) })
+}
+
+public fun Fragment.bind<T>(from: Observable<T>, to: Action1<in T>) {
     from.subscribe(Action1 { to.call(it) })
 }
 
