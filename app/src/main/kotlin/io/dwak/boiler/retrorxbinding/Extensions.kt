26b81331593f2cde973f7adc763ac974482f7460
Vishnu Rajeevan <vrajeevan@roundarch.com>
2015-08-05 16:15:28 -0500

    small cleanup

--- app/src/main/kotlin/io/dwak/boiler/retrorxbinding/Extensions.kt
+++ app/src/main/kotlin/io/dwak/boiler/retrorxbinding/Extensions.kt
@@ -12,4 +12,3 @@ public fun Activity.bind<T>(from: Observable<T>, to: Action1<in T>) {
 public fun Fragment.bind<T>(from: Observable<T>, to: Action1<in T>) {
     from.subscribe(to)
 }
-
--- app/src/main/kotlin/io/dwak/boiler/retrorxbinding/MainActivity.kt
+++ app/src/main/kotlin/io/dwak/boiler/retrorxbinding/MainActivity.kt
@@ -3,9 +3,6 @@ package io.dwak.boiler.retrorxbinding
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ProgressBar
-import android.widget.SeekBar
-import android.widget.TextView
 import com.jakewharton.rxbinding.view.clicks
 import com.jakewharton.rxbinding.widget.changes
 import com.jakewharton.rxbinding.widget.progress
@@ -19,16 +16,12 @@ public class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.activity_main)
         val mainViewModel = MainViewModel()
-        val intToString: (Int) -> String = { it.toString() }
-
-        bind(mainViewModel.counter().map(intToString), binding.counterOutput.text())
-
+        val intToString : (Int) -> String = { it.toString() }
+        
+        bind(from = mainViewModel.counter().map(intToString), to = binding.counterOutput.text())
         bind(mainViewModel.progress().map(intToString), binding.seekbarOutput.text())
-
         bind(mainViewModel.progress(), binding.progBar.progress())
-
         bind(binding.counterButton.clicks(), mainViewModel.counterClicked())
-
         bind(binding.seekbar.changes(), mainViewModel.seekProgress())
     }
 }
